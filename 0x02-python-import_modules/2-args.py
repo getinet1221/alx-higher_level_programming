b11e75b713b96886d43003668367bbd9409704a1
Getinet Amare <getinet.dtu@outlook.com>
2021-11-18 16:15:29 +0300

    Adding a file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-def main():
-    print('{} argument'.format(len(argv) - 1), end='')
-    if len(argv) == 1:
-        print('s.')
-    elif len(argv) == 2:
-        print(':')
-    else:
-        print('s:')
-    for i in range(1, len(argv)):
-        print('{}: {}'.format(i, argv[i]))
 if __name__ == "__main__":
-    main()
+    from sys import argv
+    '''argument = "argument:" if len(argv) == 2 else
+    ("argument." if len(argv) == 1 else "arguments:")'''
+    if len(argv) == 2:
+        argument = "argument:"
+    elif len(argv) == 1:
+        argument = "arguments."
+    else:
+        argument = "arguments:"
+
+    print(len(argv) - 1, "{}".format(argument))
+    for i in range(1, len(argv)):
+        print("{}:".format(i), str(argv[i]))
