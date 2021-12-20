4d3efbd7c445a32c11ee1c3cf58e427d06a27498
Getinet Amare <getinet.dtu@outlook.com>
2021-12-20 15:24:24 +0300

    Adding a file

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-"""Function add_attribute
-"""
-
-
-def add_attribute(a_class, name, value):
-    """Adds new attribute to an object if it's possible
-    """
-
-    # Set for O(1) membership test
-    cannot_add = {int, str, float, list, dict, tuple, frozenset, type, object}
-
-    if type(a_class) in cannot_add:
+def add_attribute(obj, name, value):
+    if hasattr(obj, '__dict__') is False:
         raise TypeError("can't add new attribute")
-
-    a_class.__setattr__(name, value)
+    else:
+        setattr(obj, name, value)
