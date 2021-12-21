c075f9efba086ea6cc5ef5d2e8df548b5d646a51
Getinet Amare <getinet.dtu@outlook.com>
2021-12-21 16:48:23 +0300

    Adding a 10-student.py file

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,8 +16,3 @@ class Student:
             return {k: v for k, v in self.__dict__.items() if k in attrs}
         else:
             return self.__dict__.copy()
-
-    def reload_from_json(self, json):
-        '''Loads attributes from json.'''
-        for key, value in json.items():
-            self.__dict__[key] = value
