40cf6e12920918213076d857d2a5751a1872ca95
Getinet Amare <getinet.dtu@outlook.com>
2022-03-18 17:55:38 -0700

    Modifying 10-my_github.py file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Uses the GitHub API to display a GitHub ID based on given credentials.
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
@@ -11,5 +10,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
+    r = requests.get("https://api.github.com/getinet1221", auth=auth)
     print(r.json().get("id"))
